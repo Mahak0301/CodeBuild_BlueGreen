2cd728ce0bfe5b8f6adc2a6106f264fb164cc44d
Mahak0301 <74810461+Mahak0301@users.noreply.github.com>
2024-05-29 18:40:35 +0530

    Update before_install.sh

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -26,8 +26,8 @@
 #   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # sudo apt-get update
 # sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-cd /opt/CodeBuild_BlueGreen
-sudo docker build -t node_app .
+# cd /opt/CodeBuild_BlueGreen
+# sudo docker build -t node_app .
 # aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 205429787317.dkr.ecr.ap-south-1.amazonaws.com
 # docker pull 205429787317.dkr.ecr.ap-south-1.amazonaws.com/msw_ecr_repo:latest
-sudo docker run -itd -p 3000:3000 node_app:latest
+# sudo docker run -itd -p 3000:3000 node_app:latest
